eb802f691286a841fc54d3f471fdc3f42d07d5e6
Daniel de Abreu Lessi <daniellessi@gmail.com>
2022-11-07 15:39:01 -0300

    Inclusão dos arquivos HTML
    Inclusão dos arquivos HTML

--- Conexao360/src/main/java/br/com/recode/model/Doacao.java
+++ Conexao360/src/main/java/br/com/recode/model/Doacao.java
@@ -1,10 +1,21 @@
 package br.com.recode.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder	
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Doacao {
 	
 	@Id
